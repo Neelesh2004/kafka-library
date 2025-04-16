be8553184881dac6bc53571946cc45050d15df7f
Neelesh Gamage <neelesh.gamage@ifs.com>
2025-04-16 10:04:42 +0530

    testing

--- kafka-repo.go
+++ kafka-repo.go
@@ -15,6 +15,8 @@ type KafkaRepository struct {
 
 // Initialize a new KafkaRepository
 func NewKafkaRepository() (*KafkaRepository, error) {
+
+	fmt.Println("Creating new kafka repository")
 	brokers := "localhost:9092"
 
 	opts := []kgo.Opt{
@@ -32,32 +34,3 @@ func NewKafkaRepository() (*KafkaRepository, error) {
 
 	return &KafkaRepository{client: client}, nil
 }
-
-
-// //Produces a message using an existing Kafka client
-// func Produce(client *kgo.Client, data interface{}, topic string, key string) error {
-// 	recordData, err := json.Marshal(data)
-// 	if err != nil {
-// 		fmt.Printf("Failed to marshal request to JSON: %v\n", err)
-// 		return err
-// 	}
-
-// 	kafkaRecord := &kgo.Record{
-// 		Topic:     topic,
-// 		Key:       []byte(key),
-// 		Value:     recordData,
-// 		Timestamp: time.Now().UTC(),
-// 		Headers: []kgo.RecordHeader{
-// 			{Key: "Test-Header", Value: []byte("Sent from Test-Application")},
-// 		},
-// 	}
-
-// 	if err := client.ProduceSync(context.Background(), kafkaRecord).FirstErr(); err != nil {
-// 		fmt.Printf("Failed to produce record: %v\n", err)
-// 		return err
-// 	}
-
-// 	fmt.Printf("Published the message to topic %v\n", topic)
-
-// 	return nil
-// }
